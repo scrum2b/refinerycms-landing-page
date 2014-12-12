a81008497089006ae3ff8469af58848d1d33b9d4
Thai Nguyen Quang <thainq@ithanoi.com>
2014-12-12 18:34:00 +0700

    show module page big slider, small slider, default

--- app/controllers/refinery/page_modules/admin/page_modules_controller.rb
+++ app/controllers/refinery/page_modules/admin/page_modules_controller.rb
@@ -8,19 +8,21 @@ module Refinery
 
         def new_big_slider
         	@page_module = PageModule.new
+        	@page_module_image = PageModuleImage.new
         end
 
         def new_small_slider
         	@page_module = PageModule.new
         	@page_module_image = PageModuleImage.new
-        	@page_module_image1 = PageModuleImage.new
+          @page_module_image1 = PageModuleImage.new
         end
 
         def save_small_slider
         	@page_module = PageModule.new( :title => params[:page_module][:title], :type_module => params[:page_module][:type_module] )
 					if @page_module.valid? && @page_module.save
 						@page_module_image = PageModuleImage.new( :page_module_id => @page_module.id, :photo_id => params[:page_module][:page_module_image][:photo_id] )
-
+						@page_module_image.save if @page_module_image.valid?
+						@page_module_image = PageModuleImage.new( :page_module_id => @page_module.id, :photo_id => params[:page_module][:page_module_image][:page_module_image][:photo_id] )
 						@page_module_image.save if @page_module_image.valid?
 						redirect_to "/refinery/page_modules"
 					else
